17d2df0ee69088d68ac40830461f9de5b44d7fe6
huwenfan <huwenfan@fuliaoyi.com>
2022-01-26 22:47:42 +0800

    try add create_extranl_easybot.cmake, but not work fornow

---
+++ cmake/create_external_easybot.cmake
@@ -0,0 +1,12 @@
+# how to check file exist.
+
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../external/easybot")
+else()
+    message("Begin create a symbol link to to easybot")
+    execute_process(
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/.."
+            COMMAND "mkdir external"
+            COMMAND "ln -s ../easybot external/easybot"
+    )
+endif()
+
