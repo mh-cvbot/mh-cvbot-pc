bc28d2f60da9ec63a3d1d13e1d424c347fabea42
huwenfan <huwenfan@fuliaoyi.com>
2022-01-27 17:00:48 +0800

    up

--- cmake/create_external_easybot.cmake
+++ cmake/create_external_easybot.cmake
@@ -1,5 +1,6 @@
 # how to check file exist.
 
+message("${CMAKE_CURRENT_SOURCE_DIR}")
 if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../external/easybot")
 else()
     message("Begin create a symbol link to to easybot")
