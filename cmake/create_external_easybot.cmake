9dd4d845229d288c04c4466ffb636edc7cfdb04e
huhuang03 <huhuang03@gmail.com>
2022-09-24 00:48:22 +0800

    update check easybot

--- cmake/create_external_easybot.cmake
+++ cmake/create_external_easybot.cmake
@@ -1,9 +1,7 @@
 # how to check file exist.
 
-message("${CMAKE_CURRENT_SOURCE_DIR}")
-if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../external/easybot")
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/external/easybot")
 else()
-    message("Begin create a symbol link to to easybot")
     execute_process(
             WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/.."
             COMMAND "mkdir external"
